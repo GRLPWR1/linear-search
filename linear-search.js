415878fc3d48400ba9b132972c470874582aeace
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-04-15 02:08:23 +0300

    task 2 done: wrote functions, added and passed tests

--- linear-search.js
+++ linear-search.js
@@ -2,14 +2,32 @@
 //принимает два аргумента: объект и массив. Она должна последовательно перебирать каждый элемент и возвращать индекс,
 // в котором объект был изначально найден. Если объект не найден, то функция возвращает значение -1.
 function linearSearch(n, arr) {
-
+  if (arr.includes(n) == false) {
+    return -1;
+  }
+  for (let i=0; i < arr.length; i++) {
+    if (arr[i] === n) {
+      return i;
+    } 
+  }
 }
+console.log(linearSearch(3, [1,2,3,4,5,3]))
 
 
 //напишем новый метод globalLinearSearch, который возвращает массив всех индексов, в которых найден элемент.
 function globalLinearSearch(n, arr) {
-
+  let result = [];
+  if (arr.includes(n) == false) {
+    result = -1;
+  }
+  for (let i=0; i < arr.length; i++){
+    if (arr[i] === n) {
+      result.push(i);
+    }
+  }
+  return result;
 }
+console.log(globalLinearSearch('a', ['b', 'a', 'n', 'a', 'n', 'a', 's']))
 
 module.exports = {
   linearSearch,
--- spec/linear-search.spec.js
+++ spec/linear-search.spec.js
@@ -6,12 +6,30 @@ describe("linearSearch()", function() {
     let arr = [1,2,3,4,5];
     expect(linearSearch(item, arr)).toEqual(2);
   });
+  it("returns ony the 1st index and ignores the rest if the item is found more than once", function(){
+    let item = 3;
+    let arr = [1,2,3,4,5,3];
+    expect(linearSearch(item, arr)).not.toBe([2, 5]);
+    expect(linearSearch(item, arr)).not.toEqual(5);
+  });
+  it("returns -1 if cannot find the item in the array", function(){
+    let item = 6
+    let arr = [1,2,3,4,5];
+    expect(linearSearch(item, arr)).toEqual(-1);
+  });
 });
 
 describe("globalLinearSearch()", function() {
-  it("returns array indxes of items in array", function() {
+  it("returns an array of indices of items in the array", function() {
     let item = 'a';
     let arr = ['b', 'a', 'n', 'a', 'n', 'a', 's'];
     expect(globalLinearSearch(item, arr)).toEqual([1, 3, 5]);
+  });
+  it("returns -1 if cannot find the item in the array",() => {
+    let item = 'z';
+    let arr = ['b', 'a', 'n', 'a', 'n', 'a', 's'];
+    expect(globalLinearSearch(item, arr)).toBe(-1);
+    expect(globalLinearSearch(item, arr)).not.toBe([]);
+    expect(globalLinearSearch(item, arr)).not.toBe(undefined);
   });
 });
